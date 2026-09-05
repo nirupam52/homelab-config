55a9968a668498089d15e4ffda9a235f2e867fc4
Nirupam Bidikar <nirupam52@gmail.com>
2026-09-05 00:10:30 -0500

    testing services on tailscale

--- config/setup/tailscale/serve.sh
+++ config/setup/tailscale/serve.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eu
 
-tailscale serve --bg --https=443 http://127.0.0.1:8080
-tailscale serve --bg --https=8443 http://127.0.0.1:8081
+tailscale serve --bg --service=svc:dozzle --https=443 http://127.0.0.1:8080
+tailscale serve --bg --service=svc:pihole --https=443 http://127.0.0.1:8081/admin
 tailscale serve status
